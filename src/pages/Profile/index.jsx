98571ff077cc8213e990ac69ba1d1071bde71ed0
João Gabriel <joaogabriel9633@gmail.com>
2023-08-06 00:56:48 -0300

    Add image avatar

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,58 +1,84 @@
+import { useContext, useState } from 'react'
 import Header from '../../components/Header'
 import Title from '../../components/Title'
-import './profile.css'
+
 import { FiSettings, FiUpload } from 'react-icons/fi'
-import avatarImg from '../../assets/avatar.png'
-import { AuthContext } from '../../contexts/auth'
-import { useContext, useState } from 'react'
+import avatar from '../../assets/avatar.png'
+import {AuthContext} from '../../contexts/auth'
 
-export default function Profile() {
-    const { user } = useContext(AuthContext)
-    const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl)
+import './profile.css';
+
+export default function Profile(){
+
+  const { user, storageUser, setUser, logout } = useContext(AuthContext);
+
+  const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl)
+  const [imageAvatar, setImageAvatar] = useState(null);
+
+  const [nome, setNome] = useState(user && user.nome)
+  const [email, setEmail] = useState(user && user.email)
+
+  function handleFile(e){
+    if(e.target.files[0]){
+      const image = e.target.files[0];
+
+      if(image.type === 'image/jpeg' || image.type === 'image/png'){
+        setImageAvatar(image)
+        setAvatarUrl(URL.createObjectURL(image))
+      }else{
+        alert("Envie uma imagem do tipo PNG ou JPEG")
+        setImageAvatar(null);
+        return;
+      }
 
 
-    return (
-        <div>
-            <Header />
+    }
+  }
 
-            <div className='content'>
-                <Title name="Minha Conta" >
-                    <FiSettings size={25} />
-                </Title>
 
-                <div className="container">
+  return(
+    <div>
+      <Header/>
 
-                    <form onSubmit={e => e.preventDefault()} className='form-profile'>
+      <div className="content">
+        <Title name="Minha conta">
+          <FiSettings size={25} />
+        </Title>
 
-                        <label className='label-avatar'>
-                            <span>
-                                <FiUpload color='#fff' size={25} />
-                            </span>
+       <div className="container">
 
-                            <input type="file" accept='image/*' /> <br />
-                            {avatarUrl === null ? (
-                                <img src={avatarImg} alt="profile" width={250} height={250} />
-                            ) : (
-                                <img src={user.avatarUrl} alt="profile" width={250} height={250} />
-                            )}
-                        </label>
+        <form className="form-profile">
+          <label className="label-avatar">
+            <span>
+              <FiUpload color="#FFF" size={25} />
+            </span>
 
-                        <label>Nome</label>
-                        <input type="text" placeholder='Seu nome' />
+            <input type="file" accept="image/*" onChange={handleFile}  /> <br/>
+            {avatarUrl === null ? (
+              <img src={avatar} alt="Foto de perfil" width={250} height={250} />
+            ) : (
+              <img src={avatarUrl} alt="Foto de perfil" width={250} height={250} />
+            )}
 
-                        <label>Email</label>
-                        <input type="text" placeholder='teste@teste.com' disabled={true} />
+          </label>
 
-                        <button>Salvar</button>
-                    </form>
+          <label>Nome</label>
+          <input type="text" value={nome} onChange={(e) =>  setNome(e.target.value)}/>
 
-                </div>
+          <label>Email</label>
+          <input type="text" value={email} disabled={true} />
+          
+          <button type="submit">Salvar</button>
+        </form>
 
-                    <div className='container'>
-                        <button className='btn__logout'>Sair</button>
-                    </div>
-                    
-            </div>
-        </div>
-    )
+       </div>
+
+       <div className="container">
+         <button className="logout-btn" onClick={ () => logout() }>Sair</button>
+       </div>
+
+      </div>
+
+    </div>
+  )
 }
